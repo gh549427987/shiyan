a04368a78e75d75fa56ebead9aa645244abf3ef0
gh549427987 <549427987@qq.com>
2020-04-18 15:58:04 +0800

    4_18

--- recognize_launcher/python_captcha/crack.py
+++ recognize_launcher/python_captcha/crack.py
@@ -1,4 +1,6 @@
 from PIL import Image
+import hashlib
+import time
 
 im = Image.open("captcha.gif")
 im.convert("P")
@@ -8,6 +10,7 @@ print(im.size)
 #print(im.histogram())
 
 his = im.histogram()
+print(his)
 values = {}
 
 for i in range(256):
@@ -26,3 +29,89 @@ for x in range(im.size[1]):
             im2.putpixel((y,x),0)
 im2.save('new.gif')
 
+inletter = False
+foundletter = False
+start = 0
+end = 0
+
+letters = []
+
+for y in range(im2.size[0]):
+    for x in range(im2.size[1]):
+        pix = im2.getpixel((y,x))
+        if pix != 255:
+            inletter = True
+
+    if foundletter == False and inletter == True:
+        foundletter = True
+        start = y
+
+    if foundletter == True and inletter == False:
+        foundletter = False
+        end = y
+        letters.append((start,end))
+
+    inletter = False
+print(letters)
+
+count = 0
+for letter in letters:
+    im3 = im2.crop(( letter[0], 0, letter[1], im2.size[1]))
+    im3.save("./%s.gif"%(count))
+    count += 1
+
+import math
+
+class VectorCompare:
+    def magnitude(self, concordance):
+        total = 0
+        for word,count in concordance.items():
+            total += count ** 2
+        return math.sqrt(total)
+
+    def relation(self, concordance1, concordance2):
+        relevance = 0
+        topvalue = 0
+        for word, count in concordance1.items():
+            if word in concordance2:
+                topvalue += count * concordance2[word]
+        return topvalue / (self.magnitude(concordance1)*self.magnitude(concordance2))
+
+def buildvector(im):
+    d1 = {}
+    count = 0
+    for i in im.getdata():
+        d1[count] = i
+        count += 1
+ #   print("---------------")
+#    print("type of im.getdata() {}".format(im.getdata))
+    return d1
+  #  print("---------------")
+
+v = VectorCompare()
+
+iconset = ['0','1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+import os
+imageset = []
+for letter in iconset:
+    for img in os.listdir('./iconset/%s/'%(letter)):
+        temp = []
+        if img != "Thumbs.db" and img != ".DS_Store":
+            temp.append(buildvector(Image.open("./iconset/%s/%s"%(letter,img))))
+        imageset.append({letter:temp})
+
+count = 0
+
+for letter in letters:
+    m = hashlib.md5()
+    im3 = im2.crop((letter[0], 0, letter[1], im2.size[1]))
+    guess = []
+
+    for image in imageset:
+        for x,y in image.items():
+            if len(y) != 0:
+                guess.append((v.relation(y[0],buildvector(im3)),x))
+    guess.sort(reverse=True)
+    print(guess[0])
+    count += 11
